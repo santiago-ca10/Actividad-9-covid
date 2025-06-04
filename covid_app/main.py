bdda3a6f248bee65dbc86b84dda7d44d629b7430
SCA10 <santycardal2019@gmail.com>
2025-06-03 23:20:38 -0500

    Comentarios

--- covid_app/main.py
+++ covid_app/main.py
@@ -1,6 +1,9 @@
+# Importa la clase MainWindow desde el módulo view.main_window
 from view.main_window import MainWindow
 
+# Verifica si el archivo se está ejecutando como programa principal
 if __name__ == "__main__":
+    # Crea una instancia de la ventana principal de la aplicación
     app = MainWindow()
+    # Ejecuta la aplicación
     app.run()
-
